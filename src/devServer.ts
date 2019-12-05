be9d227ad3b64bb5144d8c4f67400b96ccab84c9
Matthew Benton <m@mattb.tech>
2019-12-05 15:23:34 +0000

    Added dev server

---
+++ src/devServer.ts
@@ -0,0 +1,36 @@
+process.env.AWS_PROFILE = "mattb.tech-deploy";
+process.env.AWS_REGION = "us-east-1";
+
+import express from "express";
+import { handler } from "./index";
+import { APIGatewayProxyEvent, APIGatewayProxyCallback } from "aws-lambda";
+import { Context } from "aws-lambda";
+
+const app = express();
+
+app.use(express.json());
+
+app.use((request, response) => {
+  const callback: APIGatewayProxyCallback = (error, val) => {
+    if (error) {
+      response.status(500).send(error);
+      return;
+    }
+    response
+      .status(200)
+      .set(val.headers)
+      .send(val.body);
+  };
+  handler(
+    {
+      httpMethod: request.method,
+      path: request.path,
+      headers: request.headers as { [name: string]: string },
+      body: JSON.stringify(request.body)
+    } as APIGatewayProxyEvent,
+    ({ callbackWaitsForEmptyEventLoop: () => {} } as unknown) as Context,
+    callback
+  );
+});
+
+app.listen(3000);
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import { ApolloServer, gql, mergeSchemas } from "apollo-server-lambda";
+import { ApolloServer, mergeSchemas } from "apollo-server-lambda";
 import githubSchema from "./githubSchema";
 import flickrSchema from "./flickrSchema";
 import goodreadsSchema from "./goodreadsSchema";
 import { promisify } from "util";
 import cache from "./cache";
 import dataSources from "./dataSources";
+import {
+  APIGatewayProxyCallback,
+  APIGatewayProxyEvent,
+  Context
+} from "aws-lambda";
 
-let handler = null;
+let realHandler = null;
 
-exports.handler = async (event, context, callback) => {
-  if (handler == null) {
+export async function handler(
+  event: APIGatewayProxyEvent,
+  context: Context,
+  callback: APIGatewayProxyCallback
+) {
+  if (realHandler == null) {
     const server = new ApolloServer({
       schema: mergeSchemas({
         schemas: await Promise.all([
@@ -26,7 +35,7 @@ exports.handler = async (event, context, callback) => {
     });
     console.log(`Server created ${JSON.stringify(server, null, 2)}`);
 
-    handler = promisify(
+    realHandler = promisify(
       server.createHandler({
         cors: {
           origin: "*",
@@ -38,11 +47,11 @@ exports.handler = async (event, context, callback) => {
   }
 
   try {
-    const response = await handler(event, context);
+    const response = await realHandler(event, context);
     console.log(`Result ${JSON.stringify(response, null, 2)}`);
     callback(null, response);
   } catch (err) {
     console.error(`Error: ${JSON.stringify(err, null, 2)}`);
     callback(err, null);
   }
-};
+}
