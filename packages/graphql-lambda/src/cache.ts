a0deb0a29e09964e16f2447383f64315f7628143
Matthew Benton <m@mattb.tech>
2020-01-31 09:30:26 +0000

    Hardcode cache table for dev server

--- packages/graphql-lambda/src/cache.ts
+++ packages/graphql-lambda/src/cache.ts
@@ -2,6 +2,6 @@ import DynamoDB from "aws-sdk/clients/dynamodb";
 import { DynamoDBCache } from "apollo-server-cache-dynamodb";
 
 export default new DynamoDBCache(new DynamoDB.DocumentClient(), {
-  tableName: process.env.CACHE_TABLE,
+  tableName: process.env.CACHE_TABLE || "Api-CacheTableC1E6DF7E-706SL5PIMYKX",
   defaultTTL: 60 * 60
 });
