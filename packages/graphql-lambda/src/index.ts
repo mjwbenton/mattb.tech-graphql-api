26d1fe22faed643f9f97d584b49f0964990ce6c0
Matthew Benton <m@mattb.tech>
2020-04-23 08:25:10 +0100

    Long cache for persisted queries

--- packages/graphql-lambda/src/index.ts
+++ packages/graphql-lambda/src/index.ts
@@ -8,12 +8,17 @@ import goodreadsSchema from "./goodreadsSchema";
 import cache from "./cache";
 import dataSources from "./dataSources";
 
+const THIRTY_DAYS = 60 * 60 * 24 * 30;
+
 const server = new ApolloServer({
   schema: mergeSchemas({
     schemas: [flickrSchema, githubSchema, goodreadsSchema]
   }),
   dataSources,
-  cache
+  cache,
+  persistedQueries: {
+    ttl: THIRTY_DAYS
+  }
 });
 
 export const handler = server.createHandler({
