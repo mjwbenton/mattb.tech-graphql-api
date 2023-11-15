c1f97b410eb3e0e4d48938b3048eb2c28f719f32
Matthew Benton <m@mattb.tech>
2023-11-15 20:15:43 +0000

    Cleanup gateway index

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -8,17 +8,13 @@ import { readFileSync } from "fs";
 
 const supergraphSdl = readFileSync("./supergraph.graphql").toString();
 
-// Initialize an ApolloGateway instance and pass it
-// the supergraph schema as a string
-
 const gateway = new ApolloGateway({
   supergraphSdl,
 });
 
-// Pass the ApolloGateway to the ApolloServer constructor
-
 const server = new ApolloServer({
   gateway,
+  // TODO: Add persisted queries and cache
 });
 
 export const handler = startServerAndCreateLambdaHandler(
