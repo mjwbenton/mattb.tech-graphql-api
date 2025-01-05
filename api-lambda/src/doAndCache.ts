0b2ea5c64c20a9924bfefb75ba11f624d6faed4c
Matthew Benton <m@mattb.tech>
2025-01-05 17:25:50 +0000

    Reenable cache

--- api-lambda/src/doAndCache.ts
+++ api-lambda/src/doAndCache.ts
@@ -3,15 +3,15 @@ import { KeyValueCache } from "@apollo/utils.keyvaluecache";
 export default async function doAndCache<T>(
   cache: KeyValueCache,
   cacheKey: string,
-  method: () => Promise<T>,
+  method: () => Promise<T>
 ): Promise<T> {
-  /*const cacheResult = await cache.get(cacheKey);
+  const cacheResult = await cache.get(cacheKey);
   if (cacheResult) {
     console.log(`Cache hit on ${cacheKey}`);
     return JSON.parse(cacheResult) as T;
   }
-  console.log(`Cache miss on ${cacheKey}`);*/
+  console.log(`Cache miss on ${cacheKey}`);
   const realResult = await method();
-  //cache.set(cacheKey, JSON.stringify(realResult));
+  cache.set(cacheKey, JSON.stringify(realResult));
   return realResult;
 }
